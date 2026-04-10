00d9d2653431ebd6b5feebd39b0272f662cbd9ba
nycollassobolevski <sobolevski.academy@gmail.com>
2026-04-10 15:56:49 -0300

    add

--- rPlace/src/app/features/main-page/main-page.ts
+++ rPlace/src/app/features/main-page/main-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IPixel } from './components/pixel/IPixel';
 import { Pixel } from "./components/pixel/pixel";
+import { PixelApi } from '../../domain/pixel.api';
 
 @Component({
   selector: 'app-main-page',
@@ -11,18 +12,21 @@ import { Pixel } from "./components/pixel/pixel";
 })
 export class MainPage {
   //FORMA ERRADA DE SE FAZER (porem funciona)  
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private api: PixelApi
+  ){}
 
 
   ngOnInit(){
     if(!sessionStorage.getItem('token'))
       this.router.navigate(['login'])
 
-    
+
     let lines = [];
-    for (let y = 0; y < 50; y++) {
+    for (let y = 0; y < 100; y++) {
       let row : IPixel[] = [];
-      for(let x = 0; x < 50; x++ ) {
+      for(let x = 0; x < 100; x++ ) {
         row.push({
             color: 'gray',
             x: x,
@@ -38,7 +42,7 @@ export class MainPage {
   protected pixels: IPixel[][] = []
 
   loadData(){
-    const received: IPixel[] = [
+    let received: IPixel[] = [
       {x:1,y:1,color:'#111',lastChange:new Date()},
       {x:2,y:1,color:'#634444',lastChange:new Date()},
       {x:3,y:1,color:'#aa9c9c',lastChange:new Date()},
@@ -50,13 +54,22 @@ export class MainPage {
       {x:3,y:3,color:'#862986',lastChange:new Date()},
     ]
 
-    for (let y = 0; y < 50; y++) {
-      for(let x = 0; x < 50; x++ ) {
-        let exists = received.find(p => p.x == x && p.y == y);
-        if(exists)
-          this.pixels[x][y] = exists;
+    this.api.GetAll().subscribe(
+      res => {
+        console.log(res);
+        for (let y = 0; y < 100; y++) {
+          for(let x = 0; x < 100; x++ ) {
+            let exists = res.find(p => p.x == x && p.y == y);
+            console.log(exists);
+            
+            if(exists)
+              this.pixels[x][y] = exists;
+          }
+        }
       }
-    }
+    )
+
+    
   }
 
   updateData(pixel: IPixel){
